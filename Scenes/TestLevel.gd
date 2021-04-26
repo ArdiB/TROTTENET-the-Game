d85d7d5412b0c38e14c9be776bc8e92ecfb16745
ArdiB <69348871+ArdiB@users.noreply.github.com>
2021-04-26 22:58:07 +0200

    modifyed timer
    timer without "timer-node"

--- Scenes/TestLevel.gd
+++ Scenes/TestLevel.gd
@@ -3,8 +3,7 @@ extends Node2D
 onready var coin_label: Label = $GUI/VBoxContainer/Coin_label
 
 var character 
-var time_sec = 0
-var time_min = 0
+var time = 0
 
 func _ready():
 	_load_character()
@@ -12,22 +11,25 @@ func _ready():
 
 func _process(delta):
 
-		
 	check_key_input()
-
+	
+	time += delta
+	
+	var secs = fmod(time, 60)
+	var mins = fmod(time, 60*60) /60
+	
+	var time_passed = "%02d : %02d" % [mins,secs]
+	$GUI/VBoxContainer/Timer.text = time_passed
+	
+	
 func check_key_input():
 	if Input. is_action_just_pressed("reload"):
 		SceneLoader.goto_scene("res://Scenes/SelectionMenu.tscn")	
 
-func _on_Timer_timeout():
-	time_sec += 1
-	if time_sec == 60:
-		time_min += 1
-		time_sec -=60
-	_update_time()
-	
-func _update_time():
-	$GUI/VBoxContainer/Timer.text = str ("Time: ", time_min, ":", time_sec)
+
+
+
+
 
 func _load_character():
 	var character_scene = load (character)
