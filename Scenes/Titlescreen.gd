9435976757e6d892642ce9a5dfde5206a0fda6cb
ArdiB <69348871+ArdiB@users.noreply.github.com>
2021-05-04 20:27:16 +0200

    TweenLogo

--- Scenes/Titlescreen.gd
+++ Scenes/Titlescreen.gd
@@ -2,9 +2,8 @@ extends Control
 
 
 func _ready():
-	pass
-
-
+	$TweenLogo.interpolate_property($Logo, "position", Vector2(160, -120), Vector2(160, 110), 2, Tween.TRANS_QUAD, Tween.EASE_IN_OUT)
+	$TweenLogo.start()
 func _process(delta):
 	_continue()
 	
@@ -13,3 +12,7 @@ func _continue():
 	if Input. is_action_just_pressed("ACCEPT"):
 		SceneLoader.goto_scene("res://Scenes/SelectionMenu.tscn")
 		#get_tree(). change_scene("res://Scenes/TestLevel.tscn")
+
+
+func _on_TweenLogo_tween_all_completed():
+	$PressEnter.visible = true
