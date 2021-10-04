a5c27227939a525b84de4228fae8edd17c5cefcc
ArdiB <69348871+ArdiB@users.noreply.github.com>
2021-10-04 22:26:23 +0200

    new assets

--- Items/Coin.gd
+++ Items/Coin.gd
@@ -12,4 +12,9 @@ func _ready():
 
 func _on_Coin_body_entered(body):
 	emit_signal("on_collect", value)
-	queue_free()
+	$AnimationPlayer.play("disappear")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "disappear":
+		queue_free()
